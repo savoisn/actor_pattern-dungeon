f9e278ee11975cdaae3d8867ad597208472deb19
Nicolas Savois <nicolas.savois@gmail.com>
2019-10-29 22:23:07 +0100

    princess attack monster and they both die

--- lib/dungeon/princess.ex
+++ lib/dungeon/princess.ex
@@ -6,4 +6,31 @@ defmodule Dungeon.Princess do
   def start() do
     spawn(fn -> hi() end)
   end
+
+  # API
+  def start_killing_machine() do
+    spawn(fn -> wait_for_orders() end)
+  end
+
+  def attack_with_sword(princess, monster) do
+    send(princess, {:attack_with_sword, monster})
+  end
+
+  def attack_with_mace(princess, monster) do
+    case Process.alive?(princess) do
+      true -> send(princess, {:attack_with_mace, monster})
+      false -> raise "Princess dead"
+    end
+  end
+
+  # Server
+  def wait_for_orders() do
+    receive do
+      {:attack_with_sword, monster} ->
+        send(monster, {:attacked})
+
+      {:attack_with_mace, monster} ->
+        send(monster, {:attacked})
+    end
+  end
 end
--- test/dungeon_test.exs
+++ test/dungeon_test.exs
@@ -1,4 +1,19 @@
 defmodule DungeonTest do
+  alias Dungeon.Princess
+  alias Dungeon.Monster
   use ExUnit.Case
-  doctest Dungeon
+
+  test "Princes attacks monster" do
+    monster = Monster.start(3)
+    assert Process.alive?(monster)
+    princess = Princess.start_killing_machine()
+    assert Process.alive?(princess)
+    Princess.attack_with_sword(princess, monster)
+    :timer.sleep(2)
+    refute Process.alive?(monster)
+
+    assert_raise RuntimeError, "Princess dead", fn ->
+      Princess.attack_with_mace(princess, monster)
+    end
+  end
 end
